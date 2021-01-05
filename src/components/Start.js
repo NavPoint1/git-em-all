b8fafddf9c229742334ab5c0c0fbd63b02580c73
NavPoint1 <foleym156@gmail.com>
2021-01-05 17:56:25 -0500

    setup for audio

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,11 +3,20 @@ import React, {useEffect} from 'react'
 import start from '../assets/start2.png'
 
 const Start = props => {
+
+    useEffect(() => {
+        const audioEl = document.querySelector(".intro-tunes")
+        audioEl.play()
+    },[])
+
     return (
         <div id="logo-wrapper">
                 <img src={start} id="start-button"/>
                 <div id="logo">
                 </div>
+                <audio className="intro-tunes">
+                    <source src="https://assets.coderrocketfuel.com/pomodoro-times-up.mp3"></source>
+                </audio>
         </div>
     )
 }
